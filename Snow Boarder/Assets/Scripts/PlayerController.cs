de3ff18945e29eb1fe84cc7815abff8c06d05342
DudeThatsHip <86862723+DudeThatsHip@users.noreply.github.com>
2022-09-05 11:48:31 -0400

    Tuned the game and added FindObjectOfType

--- Snow Boarder/Assets/Scripts/PlayerController.cs
+++ Snow Boarder/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +7,21 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
+
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Gets access to an component within the inspector under Rigidbody2D
         rb2d = GetComponent<Rigidbody2D>();
+
+        // Gets access to an component from a different object thats
+        // under SurfaceEffector 2D
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
@@ -19,12 +29,35 @@ public class PlayerController : MonoBehaviour
     {
         // Rotate an object around its Y (upward) axis in response to
         // left/right controls
-        
-        if(Input.GetKey(KeyCode.LeftArrow))
+        RotatePlayer();
+
+        // 
+        RespondToBoost();
+    }
+
+    void RespondToBoost()
+    {
+        // if we push up, then push up
+        // otherwise stay at normal speed
+        // access the SurfaceEffector2d and change its speed
+        if (Input.GetKey(KeyCode.UpArrow))
         {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            // this allows to use the component under Rigidbody2D
             rb2d.AddTorque(torqueAmount);
         }
-        else if(Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             rb2d.AddTorque(-torqueAmount);
         }
